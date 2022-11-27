278b8a48011543342208558f2004cda8dd9c5353
Tatyana <kta995@mail.ru>
2022-11-27 12:24:01 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+//от 1 до N.
+
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+//модуль Math
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i <= n)
+{
+    Console.Write(i * i + " ");
+    i++;
+}
+
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+//от 1 до N.
+
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+//модуль Math
+
+Console.WriteLine("Напишите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Квадрат чисел равен: ");
+
+if (N < 0) N = -N;
+else if (N == 0) Console.WriteLine("0 ");
+for (int i = 1; i <= N; i++)
+{
+    int sqrt = (i * i);
+    Console.Write($"{sqrt} ");
+}
