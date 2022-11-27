d35d2a4c664c4a4910c748757a6ac6637163907a
Tatyana <kta995@mail.ru>
2022-11-27 12:22:27 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+void CheckFindKoord(int x)
+{
+    if (x == 1) Console.WriteLine("Диапазон равен x > 0 y > 0");
+    else if (x == 4) Console.WriteLine("Диапазон равен x > 0 y < 0");
+    else if (x == 2) Console.WriteLine("Диапазон равен x < 0 y > 0");
+    else if (x == 3) Console.WriteLine("Диапазон равен x < 0 y < 0");
+}
+
+string CheckFindKoord2(int x)  //это функция возвращаемая
+{
+    string res;
+    if (x == 1) res = "Диапазон равен x > 0 y > 0";
+    else if (x == 4) res = "Диапазон равен x > 0 y < 0";
+    else if (x == 2) res = "Диапазон равен x < 0 y > 0";
+    else if (x == 3) res = "Диапазон равен x < 0 y < 0";
+    else res = "Введите число от 1 до 4";
+    return res; //обязательная строка
+}
+try
+{
+    Console.WriteLine("Введите номер четверти");
+    int x = Convert.ToInt32(Console.ReadLine());
+    CheckFindKoord(x);
+    Console.WriteLine(CheckFindKoord2(x));
+}
+catch
+{
+    Console.WriteLine("Введите число от 1 до 4");
+}
